d5f813fd4dc222e82fd8b0c32a42213608afee69
michaelgirardet <michaelgirardet@outlook.com>
2025-01-13 12:16:44 +0100

    c'est une grande victoire

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -15,7 +15,29 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const coord_x = Number.parseInt(req.body.coord_x);
+    const coord_y = Number.parseInt(req.body.coord_y);
+
+    const tile = await tileRepository.readByCoordinates(coord_x, coord_y);
+    if (tile.length === 0) {
+      res.sendStatus(422);
+      return;
+    }
+
+    if (coord_x < 0 || coord_x > 11) {
+      res.sendStatus(422);
+      return;
+    }
+
+    if (coord_y < 0 || coord_y > 5) {
+      res.sendStatus(422);
+      return;
+    }
+    next();
+  } catch (err) {
+    next();
+  }
 };
 
 export default {
